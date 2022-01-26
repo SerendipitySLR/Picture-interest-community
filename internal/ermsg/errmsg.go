d2c17c43ffad087c5ba253dafa6006d9890530b9
levn <37262530+levnzzz@users.noreply.github.com>
2022-01-26 20:53:46 +0800

    更新错误信息

--- internal/ermsg/errmsg.go
+++ internal/ermsg/errmsg.go
@@ -36,10 +36,25 @@ var codeMsg = map[int]string{
 	SUCCESS: "OK",
 	ERROR:   "FAIL",
 
+	// 用户登陆注册模块错误
 	TOKEN_ERROR:        "token错误",
 	TOKEN_NOT_FOUND:    "无token",
 	TOKEN_FORMAT_ERROR: "token格式错误",
 	TOKEN_NOT_VALID:    "token无效或已过期",
+
+	ERROR_ACCOUNT_HAS_EXISTED: "账号已存在",
+	ERROR_ACCOUNT_NOT_EXIST:   "账号不存在",
+	ERROR_PASSWORD_ERROR:      "密码错误",
+
+	// 主页模块错误
+	POST_FORMAT_ERROR:      "帖子格式错误",
+	POST_WRITE_ERROR:       "帖子写入数据库错误",
+	UID_IS_NOT_EXIST:       "uid不存在",
+	FEED_WRITE_ERROR:       "Feed表写入错误",
+	RELATEDATA_WRITE_ERROR: "用户相关信息表写入错误",
+
+	FORWARD_FORMAT_ERROR: "转发帖子格式错误",
+	FORWARD_WRITE_ERROR:  "转发表写入错误",
 }
 
 //通过错误码获取错误信息
